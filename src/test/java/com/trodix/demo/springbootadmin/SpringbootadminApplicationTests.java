47f8f35e320324a19d320878a47e8cac1e904188
Sebastien Vallet <sebastien.vallet89@gmail.com>
2024-08-03 16:21:40 +0200

    initial commit

---
+++ src/test/java/com/trodix/demo/springbootadmin/SpringbootadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.demo.springbootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
